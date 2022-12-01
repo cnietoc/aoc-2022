63a33b19325433528bf900657c768c2a3b22f1e6
Carlos Nieto <carlos.nietodelacuesta@telefonica.com>
2022-12-01 10:13:33 +0100

    First day completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun calculateElvesCalories(input: List<String>): MutableList<Long> {
+        val elvesCalories = mutableListOf<Long>()
+        var index = 0
+        input.forEach {
+            if (it.isBlank()) {
+                index++
+            } else {
+                if (elvesCalories.getOrNull(index) == null) {
+                    elvesCalories.add(index, 0L)
+                }
+                elvesCalories[index] = elvesCalories[index] + it.toLong()
+
+            }
+        }
+        return elvesCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val elvesCalories = calculateElvesCalories(input)
+
+        return elvesCalories.max()
+    }
+
+    fun part2(input: List<String>): Long {
+        val elvesCalories = calculateElvesCalories(input)
+
+        return elvesCalories.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
